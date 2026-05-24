1de6d655b1d462af45ebd5530b2b95af7f4fad68
mars22144 <fauziadnansptr@gmail.com>
2026-05-24 09:12:07 +0700

    add poppular product

--- bakery.py
+++ bakery.py
@@ -2,4 +2,8 @@ import pandas as pd
 import matplotlib.pylab as mpatches
 import matplotlib.cm as mcm
 
-ds = pd.read_csv("csv/bakery_sales_dataset.csv")
+ds = pd.read_csv("csv/bakery_sales_dataset.csv", sep=",", encoding="latin1")
+
+pop_product = ds["Product_Name"].value_counts().idxmax()
+
+print(pop_product)
