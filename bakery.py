550a53e9ee6daae1c15f9cb0c470137dd44a253f
mars22144 <fauziadnansptr@gmail.com>
2026-05-24 10:22:33 +0700

    tata letak

--- bakery.py
+++ bakery.py
@@ -4,8 +4,10 @@ import seaborn as sbn
 
 ds = pd.read_csv("csv/bakery_sales_dataset.csv", sep=",", encoding="latin1")
 
-ds["Date"] = pd.to_datetime(ds["Date"])
-
 # tren penjualan(ganti to_period sesuai kebutuhan)
+ds["Date"] = pd.to_datetime(ds["Date"])
 penjualan = ds.groupby(ds["Date"].dt.to_period("M"))["Total_Sales"].sum().reset_index()
+
+
+
 print(penjualan.to_string(index=False))
