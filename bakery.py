b3e6fa9d6d5556be7e0bc5887c1f0f61ceb5d10b
mars22144 <fauziadnansptr@gmail.com>
2026-05-24 10:06:46 +0700

    y

--- bakery.py
+++ bakery.py
@@ -8,6 +8,6 @@ ds = pd.read_csv("csv/bakery_sales_dataset.csv", sep=",", encoding="latin1")
 pop_product = ds.groupby("Product_Name")["Quantity_Sold"].sum().reset_index()
 pop_product = pop_product.sort_values(by="Quantity_Sold", ascending=False)
 
-
+# print(ds)
 
 print(pop_product.to_string(index=False))
