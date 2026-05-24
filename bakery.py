3864754e9288a2ef480133f6782c1a3ca485c619
mars22144 <fauziadnansptr@gmail.com>
2026-05-24 11:26:48 +0700

    modif tren penjualan supaya di 1 figure

--- bakery.py
+++ bakery.py
@@ -19,15 +19,25 @@ categories = ds.groupby("Category")["Quantity_Sold"].sum().reset_index()
 cabang_sales = ds.groupby("Branch")["Total_Sales"].sum().reset_index()
 cabang_sales = cabang_sales.sort_values(by="Total_Sales", ascending=False)
 
-# grafik tren penjualan
-plt.figure(figsize=(10, 5))
-penjualan["Date"] = penjualan["Date"].dt.to_timestamp()
+# 1 figure 4 subplot
+fig, axes = plt.subplots(nrows=2, ncols= 2, figsize=(15, 10))
+fig.suptitle("Grafik penjualan", fontsize=16, fontweight="bold")
 
-sbn.lineplot(data=penjualan, x="Date", y="Total_Sales", marker="o", color="b")
-plt.title("Tren Penjualan Bulanan")
-plt.xlabel("Bulan")
-plt.ylabel("Total Penjualan")
-plt.grid(True)
+# grafik tren penjualan
+penjualan_subplot = penjualan.copy()
+penjualan_subplot["Date"] = penjualan_subplot["Date"].dt.to_timestamp()
+sbn.lineplot(ax=axes[0, 0], data=penjualan_subplot, x="Date", y="Total_Sales", marker="o", color="b", linewidth=2)
+axes[0, 0].set_title("Penjualan Trend Bulanan", fontsize=16, fontweight="bold")
+axes[0, 0].set_xlabel("Bulan")
+axes[0, 0].set_ylabel("Total")
+axes[0, 0].grid(True, linestyle="--", alpha=0.6)
+
+# grafik product terlaris dan kurang laris
+plt.figure(figsize=(10, 5))
+sbn.barplot(data=product_sales, x="Quantity_Sold", y="Product_Name", palette="Blues_r")
+plt.title("Grafik Penjualan Product yang laris dan yang kurang laku")
+plt.xlabel("Total")
+plt.ylabel("Product")
 
 
 print("=" * 15, "tren penjualan", "=" * 15)
@@ -38,4 +48,5 @@ print("=" * 15, "penjualan berdasarkan kategori", "=" * 15)
 print(categories.to_string(index=False))
 print("=" * 15, "penjualan tiap cabang", "=" * 15)
 print(cabang_sales.to_string(index=False))
+plt.tight_layout()
 plt.show()
